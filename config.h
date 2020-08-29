3e48aadc88398366384c707e747c429d1b015a68
Keegan K <keegan@aurora>
2020-08-28 23:37:31 -0400

    various modifications

--- config.h
+++ config.h
@@ -1,31 +1,32 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;	/* 1 means swallow floating windows */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Dina:size=13", "Source Han Sans JP:pixelsize=13:antialias=false" };
+static const char dmenufont[]       = "Dina:size=13";
+static const char col_gray1[]       = "#282828";
+static const char col_gray2[]       = "#a89984";
+static const char col_gray3[]       = "#ebdbb2";
+static const char col_gray4[]       = "#fbf1c7";
+static const char col_accnt[]       = "#458588";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_accnt, col_accnt  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+//static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -52,7 +53,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ KeyPress,   MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ KeyPress,   MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -64,7 +65,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_accnt, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
@@ -72,49 +73,53 @@ static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34
 #include "movestack.c"
 static Key keys[] = {
 	/* type		modifier                     key        function        argument */
-	{  KeyPress,	MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{  KeyPress,	MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{  KeyPress,	MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
-	{  KeyPress,	MODKEY,                       XK_b,      togglebar,      {0} },
-	{  KeyPress,	MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{  KeyPress,	MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{  KeyPress,	MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{  KeyPress,	MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{  KeyPress,	MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{  KeyPress,	MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{  KeyPress,	MODKEY|ShiftMask,	      XK_j,      movestack,      {.i = +1 } },
-	{  KeyPress,	MODKEY|ShiftMask,	      XK_k,      movestack,      {.i = -1 } },
-	{  KeyPress,	MODKEY|Mod4Mask,              XK_h,      incrgaps,       {.i = +1 } },
-	{  KeyPress,	MODKEY|Mod4Mask,              XK_l,      incrgaps,       {.i = -1 } },
-	{  KeyPress,	MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
-	{  KeyPress,	MODKEY|Mod4Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
-	{  KeyPress,	MODKEY|Mod4Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
-	{  KeyPress,	MODKEY|Mod4Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
-	{  KeyPress,	MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
-	{  KeyPress,	MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
-	{  KeyPress,	MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
-	{  KeyPress,	MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
-	{  KeyPress,	MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
-	{  KeyPress,	MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
-	{  KeyPress,	MODKEY|Mod4Mask,              XK_y,      incrohgaps,     {.i = +1 } },
-	{  KeyPress,	MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } },
-	{  KeyPress,	MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
-	{  KeyPress,	MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
-	{  KeyPress,	MODKEY,                       XK_Return, zoom,           {0} },
-	{  KeyPress,	MODKEY,                       XK_Tab,    view,           {0} },
-	{  KeyPress,	MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{  KeyPress,	MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{  KeyPress,	MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{  KeyPress,	MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{  KeyPress,	MODKEY,                       XK_space,  setlayout,      {0} },
-	{  KeyPress,	MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{  KeyPress,	MODKEY,                       XK_s,      togglesticky,   {0} },
-	{  KeyPress,	MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{  KeyPress,	MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{  KeyPress,	MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{  KeyPress,	MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{  KeyPress,	MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{  KeyPress,	MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{  KeyPress,	MODKEY,                      XK_p,      spawn,          {.v = dmenucmd } },
+	{  KeyPress,	MODKEY|ShiftMask,            XK_Return, spawn,          {.v = termcmd } },
+	{  KeyPress,	MODKEY,                      XK_grave,  togglescratch,  {.v = scratchpadcmd } },
+	{  KeyPress,	MODKEY,                      XK_b,      togglebar,      {0} },
+	{  KeyPress,	MODKEY,                      XK_j,      focusstack,     {.i = +1 } },
+	{  KeyPress,	MODKEY,                      XK_k,      focusstack,     {.i = -1 } },
+	{  KeyPress,	MODKEY,                      XK_i,      incnmaster,     {.i = +1 } },
+	{  KeyPress,	MODKEY,                      XK_d,      incnmaster,     {.i = -1 } },
+	{  KeyPress,	MODKEY,                      XK_h,      setmfact,       {.f = -0.05} },
+	{  KeyPress,	MODKEY,                      XK_l,      setmfact,       {.f = +0.05} },
+	{  KeyPress,	MODKEY|ShiftMask,	     XK_j,      movestack,      {.i = +1 } },
+	{  KeyPress,	MODKEY|ShiftMask,	     XK_k,      movestack,      {.i = -1 } },
+	{  KeyPress,	MODKEY|Mod4Mask,             XK_h,      incrgaps,       {.i = +1 } },
+	{  KeyPress,	MODKEY|Mod4Mask,             XK_l,      incrgaps,       {.i = -1 } },
+	{  KeyPress,	MODKEY|Mod4Mask|ShiftMask,   XK_h,      incrogaps,      {.i = +1 } },
+	{  KeyPress,	MODKEY|Mod4Mask|ShiftMask,   XK_l,      incrogaps,      {.i = -1 } },
+	{  KeyPress,	MODKEY|Mod4Mask|ControlMask, XK_h,      incrigaps,      {.i = +1 } },
+	{  KeyPress,	MODKEY|Mod4Mask|ControlMask, XK_l,      incrigaps,      {.i = -1 } },
+	{  KeyPress,	MODKEY|Mod4Mask,             XK_0,      togglegaps,     {0} },
+	{  KeyPress,	MODKEY|Mod4Mask|ShiftMask,   XK_0,      defaultgaps,    {0} },
+	{  KeyPress,	MODKEY,                      XK_y,      incrihgaps,     {.i = +1 } },
+	{  KeyPress,	MODKEY,                      XK_o,      incrihgaps,     {.i = -1 } },
+	{  KeyPress,	MODKEY|ControlMask,          XK_y,      incrivgaps,     {.i = +1 } },
+	{  KeyPress,	MODKEY|ControlMask,          XK_o,      incrivgaps,     {.i = -1 } },
+	{  KeyPress,	MODKEY|Mod4Mask,             XK_y,      incrohgaps,     {.i = +1 } },
+	{  KeyPress,	MODKEY|Mod4Mask,             XK_o,      incrohgaps,     {.i = -1 } },
+	{  KeyPress,	MODKEY|ShiftMask,            XK_y,      incrovgaps,     {.i = +1 } },
+	{  KeyPress,	MODKEY|ShiftMask,            XK_o,      incrovgaps,     {.i = -1 } },
+	{  KeyPress,	MODKEY,                      XK_Return, zoom,           {0} },
+	{  KeyPress,	MODKEY,                      XK_Tab,    view,           {0} },
+	{  KeyPress,	MODKEY|ShiftMask,            XK_c,      killclient,     {0} },
+	{  KeyPress,	MODKEY,                      XK_t,      setlayout,      {.v = &layouts[0]} },
+	{  KeyPress,	MODKEY,                      XK_f,      setlayout,      {.v = &layouts[1]} },
+	{  KeyPress,	MODKEY,                      XK_m,      setlayout,      {.v = &layouts[2]} },
+	{  KeyPress,	MODKEY,                      XK_space,  setlayout,      {0} },
+	{  KeyPress,	MODKEY|ShiftMask,            XK_space,  togglefloating, {0} },
+	{  KeyPress,	MODKEY,                      XK_s,      togglesticky,   {0} },
+	{  KeyPress,	MODKEY,                      XK_0,      view,           {.ui = ~0 } },
+	{  KeyPress,	MODKEY|ShiftMask,            XK_0,      tag,            {.ui = ~0 } },
+	{  KeyPress,	MODKEY,                      XK_comma,  focusmon,       {.i = -1 } },
+	{  KeyPress,	MODKEY,                      XK_period, focusmon,       {.i = +1 } },
+	{  KeyPress,	MODKEY|ShiftMask,            XK_comma,  tagmon,         {.i = -1 } },
+	{  KeyPress,	MODKEY|ShiftMask,            XK_period, tagmon,         {.i = +1 } },
+	{  KeyPress,	MODKEY,			     XK_n,	spawn,		SHCMD("xdotool keyup Super_L && xdotool click 1") }, // Left click mouse
+	{  KeyPress,	MODKEY|ShiftMask,            XK_x,	spawn,		SHCMD("slock") },
+	{  KeyPress,	MODKEY,                      XK_Print,	spawn,		SHCMD("escrotum ~/Pictures/Screenshots/%Y-%m-%d-%H%M%S_$wx$h.png && notify-send 'Screenshot saved in ~/Pictures/Screenshots'") },
+	{  KeyPress,	MODKEY|ShiftMask,            XK_Print,	spawn,		SHCMD("escrotum -s ~/Pictures/Screenshots/%Y-%m-%d-%H%M%S_$wx$h.png && notify-send 'Screenshot saved in ~/Pictures/Screenshots'") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -124,7 +129,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ KeyPress,   MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{  KeyPress,   MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
 /* button definitions */
