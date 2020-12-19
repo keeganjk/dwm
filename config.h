92cb417e065e9138558756023226ee8d9f90b2fb
jjj <jjj@x220.localdomain>
2020-12-19 15:50:43 -0500

    fixed color relation with xresources

--- config.h
+++ config.h
@@ -22,7 +22,7 @@ static char selbgcolor[]            = "#667893";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
-       [SchemeSel]  = { selfgcolor,  normbgcolor, selbordercolor  },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
 /* tagging */
