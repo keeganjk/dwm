0e7e52451977764e50ad5b4385622f3dbc635195
keeganjk <24554125+keeganjk@users.noreply.github.com>
2021-04-02 17:02:13 -0400

    reduced gap size to 0

--- config.h
+++ config.h
@@ -4,10 +4,10 @@
 static unsigned int borderpx        = 8;        /* border pixel of windows */
 static unsigned int snap            = 32;       /* snap pixel */
 static const int swallowfloating    = 1;	/* 1 means swallow floating windows */
-static const unsigned int gappih    = 25;        /* horiz inner gap between windows */
-static const unsigned int gappiv    = 25;        /* vert inner gap between windows */
-static const unsigned int gappoh    = 25;        /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 25;        /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 0;        /* horiz inner gap between windows */
+static const unsigned int gappiv    = 0;        /* vert inner gap between windows */
+static const unsigned int gappoh    = 0;        /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 0;        /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar                  = 1;        /* 0 means no bar */
 static int topbar                   = 1;        /* 0 means bottom bar */
